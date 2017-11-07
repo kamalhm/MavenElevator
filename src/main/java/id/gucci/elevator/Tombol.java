deba04cb2c2483953c6e923381f674842c0ccf3c
kamalhm <kamalhm1997@gmail.com>
2017-11-07 12:17:02 +0700

    Maven template with few template given
    Added two basic feature, no code yet, body only.
    Based on maven cucumber skeleton.

---
+++ src/main/java/id/gucci/elevator/Tombol.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes.impal.oo;
+
+/**
+ *
+ * @author Rohman
+ */
+public class Tombol {
+    private boolean nyala = false;
+
+    public boolean getNyala() {
+        return nyala;
+    }
+
+    public void setNyala(boolean nyala) {
+        this.nyala = nyala;
+    }
+    
+}
---
+++ src/main/java/id/gucci/elevator/TombolElevator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes.impal.oo;
+
+/**
+ *
+ * @author Rohman
+ */
+public class TombolElevator extends Tombol {
+    private int nomorLantai;
+
+    public void setNomorLantai(int nomorLantai) {
+        this.nomorLantai = nomorLantai;
+    }
+
+    public int getNomorLantai() {
+        return nomorLantai;
+    }
+    
+    
+}
