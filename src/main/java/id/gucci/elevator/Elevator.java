c46fbe596d9eb07da50251ec524e8255c252f369
Vina Fadriani Effendi <effendivina@gmail.com>
2017-12-18 15:02:10 +0700

    Add files via upload

--- src/main/java/id/gucci/elevator/Elevator.java
+++ src/main/java/id/gucci/elevator/Elevator.java
@@ -10,6 +10,7 @@ public class Elevator {
     private final int maxBeban;
     private final int maxOrang;
     private ArrayList<Person> listOrang;
+    private String lt = "Lantai saat ini : ";
 
     public Elevator(int lantaiSekarang, int maxOrang, int maxBeban) {
         this.lantaiSekarang = lantaiSekarang;
@@ -33,51 +34,76 @@ public class Elevator {
     }
 
     public void bergerak() {
+        ArrayList<Integer> tempvip = new ArrayList<Integer>();
         ArrayList<Integer> tempnaik = new ArrayList<Integer>();
         ArrayList<Integer> tempturun = new ArrayList<Integer>();
         if (cekStatus()) {
+            for (Person person : listOrang){
+                if (person.isVIP()){
+                    tempvip.add(person.getTujuan());
+                }
+            }
+            for (int i = 0 ; i < tempvip.size();i++){
+                if (lantaiSekarang <= tempvip.get(i)){
+                    while (lantaiSekarang != tempvip.get(i)){
+                        lantaiSekarang++;
+                        System.out.println(lt+lantaiSekarang);
+                    }
+                    System.out.println("VIP keluar");
+                }
+                else{
+                    while (lantaiSekarang != tempvip.get(i)){
+                        lantaiSekarang--;
+                        System.out.println(lt+lantaiSekarang);
+                    }
+                    System.out.println("VIP keluar");
+                }   
+            }
             for (Person person : listOrang) {
-                if (person.getTujuan() > lantaiSekarang) {
+                if (person.getTujuan() > lantaiSekarang && !person.isVIP()) {
                     tempnaik.add(person.getTujuan());
-                } else if (person.getTujuan() < lantaiSekarang) {
+                } else if (person.getTujuan() < lantaiSekarang && !person.isVIP()) {
                     tempturun.add(person.getTujuan());
                 }
             }
             Collections.sort(tempnaik);
             Collections.sort(tempturun);
+            Collections.reverse(tempturun);
 
             if (arah) {
                 for (int i = 0; i < tempnaik.size(); i++) {
                     while (lantaiSekarang != tempnaik.get(i)) {
                         lantaiSekarang++;
-                        System.out.println("Lantai saat ini " + lantaiSekarang);
-                      
+                        System.out.println(lt+ lantaiSekarang); 
                     }
-
+                    System.out.println("keluar");
                 }
                 for (int j = 0; j < tempturun.size(); j++) {
                     while (lantaiSekarang != tempturun.get(j)) {
                         lantaiSekarang--;
-                        System.out.println("Lantai saat ini " + lantaiSekarang);
+                        System.out.println(lt + lantaiSekarang);
                     }
+                    System.out.println("keluar");
                 }
                 arah = false;
             } else if (!arah) {
                 for (int i = 0; i < tempturun.size(); i++) {
                     while (lantaiSekarang != tempturun.get(i)) {
                         lantaiSekarang--;
-                        System.out.println("Lantai saat ini " + lantaiSekarang);
+                        System.out.println(lt + lantaiSekarang);
                     }
+                    System.out.println("keluar");
                 }
                 for (int j = 0; j < tempnaik.size(); j++) {
                     while (lantaiSekarang != tempnaik.get(j)) {
                         lantaiSekarang++;
-                        System.out.println("Lantai saat ini " + lantaiSekarang);
-                       
+                        System.out.println(lt+ lantaiSekarang);                       
                     }
+                    System.out.println("keluar ");
                 }
                 arah = true;
             }
+            tempvip.clear();
             tempnaik.clear();
             tempturun.clear();
             listOrang.clear();
